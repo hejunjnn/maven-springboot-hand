854dcb22bee4fc8c770594277bd1eb40a4e0d3c3
hejunjnn <hjjyn0304@163.com>
2018-12-17 20:35:35 +0800

    "this is project update3 "

--- src/main/java/com/baizhi/hjj/controller/TestController.java
+++ src/main/java/com/baizhi/hjj/controller/TestController.java
@@ -11,6 +11,7 @@ public class TestController {
     @RequestMapping("/test")
     public String Test() {
         System.out.println("hehe");
+        System.out.println("哈哈哈哈");
 
         return "hello springboot";
     }
